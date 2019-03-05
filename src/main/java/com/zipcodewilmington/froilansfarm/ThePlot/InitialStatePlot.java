42d3890f761de8e016f6ff9125fa8c99bcee2346
WilliamXu <williamxu@zipcoders-MacBook-Pro-90.local>
2019-03-05 13:48:44 -0500

    committing to share

--- src/main/java/com/zipcodewilmington/froilansfarm/ThePlot/InitialStatePlot.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/ThePlot/InitialStatePlot.java
@@ -1,11 +1,16 @@
 package com.zipcodewilmington.froilansfarm.ThePlot;
 
 
+import com.zipcodewilmington.froilansfarm.FarmStuff.Farm;
 import com.zipcodewilmington.froilansfarm.People.Farmer;
 
 import java.util.ArrayList;
 
 public class InitialStatePlot {
     ArrayList<Farmer> farmers;
-    
+    Farm farm;
+
+    public InitialStatePlot(){
+
+    }
 }
