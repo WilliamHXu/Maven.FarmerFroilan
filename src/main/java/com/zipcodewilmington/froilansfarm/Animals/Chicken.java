1a710a1ffb4fd497f85fccaf3b79ab0dbedf0ddf
WilliamXu <williamxu@zipcoders-MacBook-Pro-90.local>
2019-03-06 19:47:46 -0500

    Finished crops and animals

--- src/main/java/com/zipcodewilmington/froilansfarm/Animals/Chicken.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animals/Chicken.java
@@ -1,5 +1,6 @@
 package com.zipcodewilmington.froilansfarm.Animals;
 
+import com.zipcodewilmington.froilansfarm.Edibles.EarCorn;
 import com.zipcodewilmington.froilansfarm.Edibles.Egg;
 import com.zipcodewilmington.froilansfarm.Interfaces.Edible;
 import com.zipcodewilmington.froilansfarm.Interfaces.Produce;
@@ -8,17 +9,39 @@ public class Chicken extends Animal implements Produce {
     private boolean hasBeenFertilized;
 
     public boolean eat(Edible food) {
-        return false;
+        if (food instanceof EarCorn){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public void makeNoise() {
+        String noise = "Cuck!";
+        System.out.println(noise);
+    }
 
+    public void makeNoise2() {
+        String noise = "BABOAK!";
+        System.out.println(noise);
     }
 
     public Edible yield() {
         if(!hasBeenFertilized){
             return new Egg();
         }
-        return null;
+        else {
+            makeNoise2();
+            return null;
+        }
+    }
+
+    public boolean isHasBeenFertilized() {
+        return hasBeenFertilized;
+    }
+
+    public void setHasBeenFertilized(boolean hasBeenFertilized) {
+        this.hasBeenFertilized = hasBeenFertilized;
     }
 }
