98fc1005dc43ec9ed0e32d5154d19039afb6dcc0
Neelanjana Mukherjee <neelanjanamukherjee@zipcoders-MacBook-Pro.local>
2019-03-08 11:35:32 -0500

    vehicles

--- src/test/java/com/zipcodewilmington/froilansfarm/VehicleTests/TractorTests.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/VehicleTests/TractorTests.java
@@ -1,15 +1,49 @@
 package com.zipcodewilmington.froilansfarm.VehicleTests;
 
+import com.zipcodewilmington.froilansfarm.Animals.Chicken;
 import com.zipcodewilmington.froilansfarm.FarmStuff.Farm;
+import com.zipcodewilmington.froilansfarm.Interfaces.Edible;
+import com.zipcodewilmington.froilansfarm.Vehicle.FarmVehicles.Tractor;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class TractorTests {
 
-    public void makeNoise() {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+@Test
+    public void makeNoiseTest() {
+
+    Tractor tractor = new Tractor();
+    tractor.makeNoise();
+    String expected = "Vroom!\n";
+    String actual = outContent.toString();
+    Assert.assertEquals(expected , actual);
 
     }
 
-    public void operate(Farm farm) {
+    public void operateTest() {
 
+    }
+
+    public void harvestTest(){
+
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
     }
 
 }
