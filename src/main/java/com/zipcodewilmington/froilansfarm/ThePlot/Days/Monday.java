3385bdaf812869541bf13860cfa2c5a2f4a61ac2
Sean MacLaughlin <seanmaclaughlin@zipcoders-MacBook-Pro.local>
2019-03-10 13:58:49 -0400

    MAR1019 13:58

--- src/main/java/com/zipcodewilmington/froilansfarm/ThePlot/Days/Monday.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/ThePlot/Days/Monday.java
@@ -1,4 +1,47 @@
 package com.zipcodewilmington.froilansfarm.ThePlot.Days;
 
-public class Monday {
+import com.zipcodewilmington.froilansfarm.Crops.CornStalk;
+import com.zipcodewilmington.froilansfarm.Crops.Crop;
+import com.zipcodewilmington.froilansfarm.Crops.SoybeanPlant;
+import com.zipcodewilmington.froilansfarm.Crops.TomatoPlant;
+import com.zipcodewilmington.froilansfarm.FarmStuff.Farm;
+import com.zipcodewilmington.froilansfarm.Field.CropRow;
+import com.zipcodewilmington.froilansfarm.Vehicle.Aircraft.CropDuster;
+
+public class Monday extends DayOfWeek {
+    public static void run(Farm farm) {
+        breakfast(farm);
+        morning(farm);
+        lunch(farm);
+        afternoon(farm);
+        dinner(farm);
+    }
+
+    public static void afternoon(Farm farm) {
+        cropDust(farm, new CornStalk());
+        cropDust(farm, new TomatoPlant());
+        cropDust(farm, new SoybeanPlant());
+    }
+
+    public static void cropDust(Farm farm, Crop crop) {
+        CropDuster cropDuster = null;
+        CropRow cropRow;
+
+        froilanda.mount(cropDuster);
+
+        if (crop instanceof CornStalk) {
+            cropRow = farm.getField().get().get(0);
+            cropDuster.fertilize(cropRow);
+
+        } else if (crop instanceof TomatoPlant) {
+            cropRow = farm.getField().get().get(1);
+            cropDuster.fertilize(cropRow);
+
+        } else {
+            cropRow = farm.getField().get().get(2);
+            cropDuster.fertilize(cropRow);
+        }
+
+        froilanda.dismount(cropDuster);
+    }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/ThePlot/Days/Tuesday.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/ThePlot/Days/Tuesday.java
@@ -1,4 +1,62 @@
 package com.zipcodewilmington.froilansfarm.ThePlot.Days;
 
-public class Tuesday {
+import com.zipcodewilmington.froilansfarm.Crops.CornStalk;
+import com.zipcodewilmington.froilansfarm.Crops.Crop;
+import com.zipcodewilmington.froilansfarm.Crops.SoybeanPlant;
+import com.zipcodewilmington.froilansfarm.Crops.TomatoPlant;
+import com.zipcodewilmington.froilansfarm.Edibles.EarCorn;
+import com.zipcodewilmington.froilansfarm.Edibles.Soybean;
+import com.zipcodewilmington.froilansfarm.Edibles.Tomato;
+import com.zipcodewilmington.froilansfarm.FarmStuff.Farm;
+import com.zipcodewilmington.froilansfarm.Field.CropRow;
+import com.zipcodewilmington.froilansfarm.Vehicle.FarmVehicles.Tractor;
+
+import java.util.Set;
+
+public class Tuesday extends DayOfWeek {
+
+    public static void run(Farm farm) {
+        breakfast(farm);
+        morning(farm);
+        lunch(farm);
+        afternoon(farm);
+        dinner(farm);
+    }
+
+    public static void afternoon(Farm farm) {
+        harvestCrops(farm, new CornStalk());
+        harvestCrops(farm, new TomatoPlant());
+        harvestCrops(farm, new SoybeanPlant());
+
+    }
+    public static void harvestCrops(Farm farm, Crop crop) {
+        CropRow cropRow = null;
+        Tractor tractor = new Tractor();
+
+        froilan.mount(tractor);
+
+        if (crop instanceof CornStalk) {
+            cropRow = farm.getField().get().get(0);
+            for (int i = 200; i > 0; i--) {
+                cropRow.remove(new CornStalk());
+                farm.getSilo().addEdible(new EarCorn());
+            }
+        }
+        else if (crop instanceof TomatoPlant) {
+            cropRow = farm.getField().get().get(1);
+            for (int i = 80; i > 0; i--) {
+                cropRow.remove(new TomatoPlant());
+                farm.getSilo().addEdible(new Tomato());
+            }
+        }
+        else {
+            cropRow = farm.getField().get().get(2);
+            for (int i = 500; i > 0; i--) {
+                cropRow.remove(new SoybeanPlant());
+                farm.getSilo().addEdible(new Soybean());
+            }
+        }
+        froilan.dismount(tractor);
+    }
 }
+
